236c9195dd2668ba7c02a2b747f98b0e005b9882
nicolasvalderrama689 <nicolasvalderrama689@gmail.com>
2024-09-11 16:50:48 -0500

    se modifica el archivo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,10 +1,11 @@
 import time
 
-
+print("hola") # esta linea imprime en consola la palabra hola
 print("Hello World \n")
 
 time.sleep(3)
 print("segundo reglon")
 
-print(type("hola"))
+print(type("hola")) # la funcion type me dice que tip de datos esta en los parentecis
 print(type(4))
+print(type(5.5))
